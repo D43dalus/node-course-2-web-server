b7444bf9b753495dba94c44c027b04b3d571abbb
D43dalus <gstromfelt@gmail.com>
2017-10-28 17:49:44 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+// setting up express
+var app = express();
+
+//configuring to register partials
+hbs.registerPartials(__dirname + '/views/partials');
+//setting up hbs
+app.set('view engine', 'hbs');
+
+//registering middleware
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to sever.log')
+        }
+    });
+    next();
+});
+
+// //maintenance with no next() call - stays on this page regardless of calls
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+//identifying absolute route to /public folder
+app.use(express.static(__dirname + '/public'));
+
+//registering helpers
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+//getting page for home root
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my node.js rendered webpage',
+    })
+});
+
+//getting page for about page
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+//getting page for bad page
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'This page does not exist'
+    });
+});
+
+//app listening on port3000 for actions from user
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
